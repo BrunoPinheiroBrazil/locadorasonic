b07befa2c2a3e19dc45d15262d35bb3577efcd97
BrunoPinheiroBrazil <brunopinheirobrazil@gmail.com>
2024-08-23 18:33:07 -0300

    mais exercícios!

--- LocadoraSonic/Controllers/PalavrasController.cs
+++ LocadoraSonic/Controllers/PalavrasController.cs
@@ -6,13 +6,17 @@ namespace LocadoraSonic.Controllers
   [Route("[controller]")]
   public class PalavrasController : ControllerBase
   {
-    //Aqui deverá ter um método GET que vai receber um parâmetro chamado palavra do tipo string.
+    //Aqui deverá ter um método GET que vai receber um parâmetro chamado palavra do tipo string ([FromQuery]).
     //O caminho do GET deverá ser 'invertepalavra' (se baseia nos anteriores) 
-    //O método deverá inverter a palavra. Ex: sonic vai ser cinos. 
-    //O metodo deverá retornar a variavel com a palavra invertida. 
-    //Não mencionei as outras coisas como o método deverá ser IAction result e o resto pois 
-    //O sonic vai se basear nos outros métodos de GETS que já criou. Tudo basicamente igual. 
-    //Sonic deverá se atentar apenas as diferenças e focar no que tem que fazer. 
+    //Aqui eu já fiz o método, sonic deverá corrigir para que faça o que é pedido. 
+    //O método deverá inverter a palavra. Ex: sonic vai ser cinos, mickey vai ser yekcim, e assim por diante. 
+    //O metodo deverá retornar a variavel com a palavra invertida dentro do Ok. 
     //Boa sorte! 
+
+    [HttpGet("invertepalavra")]
+    public IActionResult InvertePalavra([FromQuery] string palavra)
+    {
+      return Ok("Vacilo!");
+    }
   }
 }
