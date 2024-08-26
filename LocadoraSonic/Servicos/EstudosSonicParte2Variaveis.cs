7185b2d9ae5bcc0f627de948bbb7818a90edd5ad
BrunoPinheiroBrazil <brunopinheirobrazil@gmail.com>
2024-08-26 19:00:12 -0300

    Mais exercícios para Sonic, procurar EstudosSonicParte2Variáveis

---
+++ LocadoraSonic/Servicos/EstudosSonicParte2Variaveis.cs
@@ -0,0 +1,39 @@
+﻿namespace LocadoraSonic.Servicos
+{
+  public class EstudosSonicParte2Variaveis
+  {
+    public int MetodoQueGeraNumeroAleatorio() //Metodo retorna int. Gerado aletoriamente de 0 a 100. 
+    {
+      var randomizer = new Random();
+      var numeroAleatorio = randomizer.Next(0, 100);
+      return numeroAleatorio;
+    }
+
+    //Complete o método que pega posição do numero numa string.
+    public int PegaPosicaoDoNumero(int[] listaNumeros)
+    {
+      throw new NotImplementedException();
+    }
+
+    //Crie um método que gere um numero aleatório de 50 a 250.
+    //Aqui.
+
+
+
+    //Crie um método que retorne a posição da palavra passada.
+
+    //O seguinte metodo chama uma função desta mesma classe aqui para gerar um texto baseado em RAIVA, FELIZ ou TRISTE
+    //Arrume o metodo para que o mesmo funcione.
+    public string CriaTexto(string nomePessoa, string tipoTexto)
+    {
+
+      //Este metodo ainda não existe, você vai ter que criar um metodo estático para isso! Que retorne string.
+      //O método é pra ser criado nesta mesma classe!
+      //O texto deverá ser criado juntando o nome da pessoa e o texto de forma criativa! use sua imaginação! 
+      //Deverá retornar 1 texto diferente para cada tipo no método GeraTexto.
+      var textoCriado = GeraTexto(tipoTexto, nomePessoa); 
+
+      return textoCriado;
+    }
+  }
+}
--- LocadoraSonic/Servicos/IEstudosSonicServices.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace LocadoraSonic.Servicos
-{
-  public interface IEstudosSonicServices
-  {
-    object BuscaNumero(int numeroBusca1, List<int> lista1);
-    List<int> InverteListaSemReverse(int[] lista1);
-  }
-}
