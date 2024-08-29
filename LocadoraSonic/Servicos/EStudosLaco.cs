b5bdc56a9658512e8a51787ad06c96799ae60cc0
FabialnMohr <fabianmohrd702@gmail.com>
2024-08-29 17:40:20 -0300

    ola

---
+++ LocadoraSonic/Servicos/EStudosLaco.cs
@@ -0,0 +1,16 @@
+﻿namespace LocadoraSonic.Servicos
+{
+  public class EStudosLaco
+  {
+    public void ExemploLacos()
+    {
+      int[] l = [1, 2, 3, 4, 5];
+      string[] s = { "day", "lol", "void", "sonic" };
+
+      for (int i = 0; i < l.Length; i++)
+      {
+        Console.WriteLine(l[i]);
+      }
+    }
+  }
+}
--- LocadoraSonic/Servicos/EstudosSonicParte2Variaveis.cs
+++ LocadoraSonic/Servicos/EstudosSonicParte2Variaveis.cs
@@ -2,7 +2,7 @@
 {
   public class EstudosSonicParte2Variaveis
   {
-    public int MetodoQueGeraNumeroAleatorio() //Metodo retorna int. Gerado aletoriamente de 0 a 100. 
+    public int MetodoQueGeraNumeroAleatorio50A250() //Metodo retorna int. Gerado aletoriamente de 0 a 100. 
     {
       var randomizer = new Random();
       var numeroAleatorio = randomizer.Next(0, 100);
@@ -10,18 +10,84 @@
     }
 
     //Complete o método que pega posição do numero numa string.
-    public int PegaPosicaoDoNumero(int[] listaNumeros)
+    public int PegaPosicaoDoNumero(int[] listaNumeros, int numeroProcurado)
     {
-      throw new NotImplementedException();
+      for (int i = 0; i < listaNumeros.Length; i++)
+      {
+        if (listaNumeros[i] == numeroProcurado)
+          return i;
+      }
+      return -1;
     }
 
     //Crie um método que gere um numero aleatório de 50 a 250.
     //Aqui.
+    public int MetodoQueGeraNumeroAleatorioEntreOsNumero50A250()
+    {
+      var randomizer = new Random();
+      var numeroAleatorio = randomizer.Next(50, 250);
+      return numeroAleatorio;
+    }
 
+    public static string GeraTexto(string tipoTexto, string nomePessoa)
+    {
+      switch (tipoTexto.ToLower())
+      {
+        case "raiva":
+          return $"o {nomePessoa} ficou com raiva pois {nomePessoa} não fez os exercicios!";
+        case "feliz":
+          return $"o {nomePessoa} ficou feliz pois {nomePessoa} fez os exercicios!";
+        case "triste":
+          return $"o {nomePessoa} ficou triste por quê não passou no teste!";
+        default:
+          return "Nada";
+      }
 
+      var teste = tipoTexto != "raiva";
 
-    //Crie um método que retorne a posição da palavra passada.
+      if (tipoTexto == "raiva")
+      {
+        return $"o {nomePessoa} esta com raiva pq não sabe fazer nada!";
 
+      }
+      else if (tipoTexto == "feliz")
+      {
+        return $"o {nomePessoa} esta feliz pq passou os teste!";
+
+      }
+      else if (tipoTexto == "triste")
+      {
+        return $"o {nomePessoa} esta tiste pq rodou!";
+
+      }
+      else
+      {
+        return "nada";
+      }
+
+      //Exemplo de Switch
+      var a = 2;
+      var b = 3;
+
+      switch (a) //Switch(parametro)
+      {
+        case 1: //Caso a seja igual a 1 cairá nesta condição.
+          if (a > b)
+            break;
+          if (a < b)
+            a = a * b;
+          a = b - a;
+          break;
+        case 2: //Caso a seja igual a 2 cairá nesta condição.
+          if (a < b)
+            break;
+          a = b - a;
+          break;
+        default: //caso não seja igual a nenhuma das condições existentes no Switch, vai cair aqui por padrão.
+          a = a;
+          break;
+      }
+    }
     //O seguinte metodo chama uma função desta mesma classe aqui para gerar um texto baseado em RAIVA, FELIZ ou TRISTE
     //Arrume o metodo para que o mesmo funcione.
     public string CriaTexto(string nomePessoa, string tipoTexto)
@@ -31,9 +97,14 @@
       //O método é pra ser criado nesta mesma classe!
       //O texto deverá ser criado juntando o nome da pessoa e o texto de forma criativa! use sua imaginação! 
       //Deverá retornar 1 texto diferente para cada tipo no método GeraTexto.
-      var textoCriado = GeraTexto(tipoTexto, nomePessoa); 
+      var textoCriado = GeraTexto(tipoTexto, nomePessoa);
 
       return textoCriado;
     }
+
+
+    //Crie um método que retorne a posição da palavra passada.
+
+
   }
 }
